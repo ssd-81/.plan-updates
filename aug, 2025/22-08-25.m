41ff66ebe393f90b0a86f639f8326a53f2f6d1ea
ssd-81 <arcsagar.91@gmail.com>
2025-08-22 19:59:04 +0530

    removed redunandant file for 2025-07 and commit for 22 aug, 2025

---
+++ aug, 2025/22-08-25.m
@@ -0,0 +1,19 @@
+## Todo/ Logs
+- [ ] solve 4 steps in the rss blog aggregator project
+- [ ] solve 5 algorithmic problems
+- [ ] do 5 sessions of rust crash course
+- [ ] complete 2 and 3 unit of rust book
+
+- Logs: x : 0; y : 0
+
+## Topics
+- backend golang
+
+
+## Notes
+- backend golang
+	- RSS feed projector
+		- fixing dependency problem 
+			- i have restructured the code and the error with circular dependency has been fixed, but i need to figure how to use the sqlc code in the handler functions
+		- i am stuck with a problem that is hardly is hardly moving and seems really hard to understand
+			- what's the trick: stick to it and question it heavily until pattern start emerging.  
